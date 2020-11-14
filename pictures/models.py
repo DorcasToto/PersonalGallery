1ef93317a9b9a014a9934dd3df02f9959c74d071
Moringa School <dorcasmosonik@gmail.com>
2020-11-14 11:34:20 +0300

    query db methods

--- pictures/models.py
+++ pictures/models.py
@@ -4,9 +4,33 @@ from django.db import models
 class Location(models.Model):
     locationName = models.CharField(max_length=30)
 
+    def saveLocation(self):
+        self.save()
+
+    def deleteLocation(self):
+        self.delete()
+
+    def updateLocation():
+        pass
+
+    def __str__(self):
+        return self.locationName
+
 
 class Category(models.Model):
     categoryName = models.CharField(max_length=30)
+
+    def saveCategory(self):
+        self.save()
+
+    def deleteCategory(self):
+        self.delete()
+
+    def updateCategory():
+        pass
+
+    def __str__(self):
+        return self.categoryName
 
 class Image(models.Model):
     imageName = models.CharField(max_length=30)
@@ -15,20 +39,26 @@ class Image(models.Model):
     imageCategory = models.ForeignKey(Category,on_delete = models.CASCADE)
     image = models.ImageField(upload_to = 'images/',default = 'images.jpg')
 
-    def saveImage():
-        pass
+    def saveImage(self):
+        self.save()
 
-    def deleteImage():
-        pass
+    def deleteImage(self):
+        self.delete()
 
     def updateImage():
         pass
 
     def getImagebyId(id):
         pass
+    
+    @classmethod
+    def searchImage(cls,category):
+        category = cls.objects.filter(imageCategory__icontains=category)
+        return category
 
-    def searchImage(category):
-        pass
+    def filterimageByLocation(cls,location):
+        location = cls.objects.filter(imageLocation = location)
+        return location
 
-    def filterimageByLocation(location):
-        pass
+    def __str__(self):
+        return self.imageName
